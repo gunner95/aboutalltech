265457e8f426f1d68a304beff710a8f0d06420c8
gunner95 <prabansal@microsoft.com>
2021-02-12 01:47:45 +0530

    Added contact form

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@
     var ssSearch = function() {
             
         var searchWrap = $('.header__search'),
-            searchField = searchWrap.find('.search-field'),
+            searchField = searchWrap.find('.header__search-field'),
             closeSearch = searchWrap.find('.header__search-close'),
             searchTrigger = $('.header__search-trigger'),
             siteBody = $('body');
@@ -322,8 +322,8 @@
                     </div>  <!-- end header__search -->
 
                     <!-- toggles -->
-                    <a href="#0" class="header__search-trigger"></a>
-                    <a href="#0" class="header__menu-toggle"><span>Menu</span></a>
+                    <!--<a href="#0" class="header__search-trigger"></a>
+                    <a href="#0" class="header__menu-toggle"><span>Menu</span></a> -->
 
                 </div>
                 <nav class="header__nav-wrap">
@@ -373,6 +373,42 @@
         }
     };
 
+    var contactSubmit = function() {
+        $('#contactForm').on('submit', function (e) {
+
+            // if the validator does not prevent form submit
+            if (!e.isDefaultPrevented()) {
+                var url = "contact.php";
+
+                // POST values in the background the the script URL
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: $(this).serialize(),
+                    success: function (data)
+                    {
+                        // data = JSON object that contact.php returns
+
+                        // we recieve the type of the message: success x danger and apply it to the 
+                        var messageAlert = 'alert-' + data.type;
+                        var messageText = data.message;
+
+                        // let's compose Bootstrap alert box HTML
+                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                        
+                        // If we have messageAlert and messageText
+                        if (messageAlert && messageText) {
+                            // inject the alert to .messages div in our form
+                            $('#contact-form').find('.messages').html(alertBox);
+                            // empty the form
+                            $('#contact-form')[0].reset();
+                        }
+                    }
+                });
+                return false;
+            }
+        })
+    };
 
 
 
@@ -384,8 +420,9 @@
         footer();
         ssPreloader();
         ssPrettyPrint();
-        ssSearch();
+        // ssSearch();
         ssMenu();
+        contactSubmit();
         ssMasonryFolio();
         ssBricksAnimate();
         ssSlickSlider();
